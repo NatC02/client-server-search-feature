3d95e26347f0afdac7fcc402cb6930079881852a
NatC02 <NatanCeballos02@gmail.com>
2022-07-20 15:32:45 -0400

    typo fix

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [animals, setAnimals] = useState([]);
 
   return (
     <main className="App">
